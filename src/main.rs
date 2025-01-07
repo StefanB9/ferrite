c99d21490bdc33e80613327b7694c4fc252ec86e
Stefan Bader <baderstefan@gmx.de>
2025-01-07 20:50:55 +0100

    added crossbeam threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{io, io::BufRead};
+
+use crossbeam::thread;
+
 fn main() {
-    println!("Hello, world!");
+    thread::scope(|scope| {
+        // Input thread
+        scope.spawn(|_| {
+            let stdin = io::stdin();
+            for line in stdin.lock().lines() {
+                match line {
+                    Ok(input) => println!("{}", input),
+                    Err(_) => {}
+                }
+            }
+            println!("Waiting for input");
+        });
+
+        // Worker thread
+        scope.spawn(|_| {
+            println!("Processing...");
+        });
+
+        // Output thread
+        scope.spawn(|_| {
+            println!("Output processor...");
+        });
+    })
+    .unwrap();
 }
